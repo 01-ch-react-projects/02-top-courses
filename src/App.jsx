0a90a61a34a0679aa8166eba6bd9e42b85c5fa88
hiteshwar <hemant.kaimwal97@gmail.com>
2024-01-12 10:53:59 +0530

    added basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Cards } from './components/Cards'
+import { Filter } from './components/Filter'
+import { Navbar } from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hi</h1>
+      <div>
+        <Navbar/>
+        
+        <Filter/>
+
+        <Cards/>
+      </div>
     </>
   )
 }
