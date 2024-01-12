5d2d7ff49c63ac8da5ce4ef12ba5cb0a472e1bf5
hiteshwar <hemant.kaimwal97@gmail.com>
2024-01-12 22:27:47 +0530

    added spinner

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,28 @@ import { Filter } from './components/Filter'
 import { Navbar } from './components/Navbar'
 import {apiUrl,filterData} from "./data";
 import { toast } from 'react-toastify';
+import { Spinner } from './components/Spinner';
 
 function App() {
 
   const[courses,setCourses]=useState([])
+  const[loading,setLoading]=useState(true)
+
+  const fetchData = async() => {
+    setLoading(true)
+    try {
+      const res=await fetch(apiUrl);
+      const respData=await res.json();
+
+      //save data into course variable
+      setCourses(respData.data)
+    } catch (error) {
+      toast.error("Error aa gai")
+    }
+    setLoading(false)
+  }
 
   useEffect( () => {
-    const fetchData = async() => {
-      try {
-        const res=await fetch(apiUrl);
-        const respData=await res.json();
-
-        //save data into course variable
-        setCourses(respData.data)
-      } catch (error) {
-        toast.error("Error aa gai")
-      }
-    }
     fetchData();
   },[])
 
@@ -33,7 +38,10 @@ function App() {
         
         <Filter filterData={filterData}/>
 
-        <Cards courses={courses}/>
+        {
+          loading ? (<Spinner/>):(<Cards courses={courses}/>)
+        }
+        
       </div>
     </>
   )
